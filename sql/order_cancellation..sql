f1612a0004ebc4f8fb3fa2ad943be3919f0a8843
enal00 <91005659+enal00@users.noreply.github.com>
2026-06-13 06:30:35 +0700

    Update order_cancellation..sql
    from  count to rate

--- sql/order_cancellation..sql
+++ sql/order_cancellation..sql
@@ -29,27 +29,33 @@ FROM
 WHERE rn > 1;
 -- Which products have the highest cancellation rates?
 
-WITH cancellation as 
-(
+WITH orders_all AS (
   SELECT
-  a.order_id,
-  b.product_id,
-  d.product_category_name_english as product_name
-
-
+    b.product_id,
+    d.product_category_name_english AS product_name,
+    a.order_id,
+    a.order_status
   FROM `amplified-cache-495313-g5.Olist.orders` a
-  LEFT JOIN `amplified-cache-495313-g5.Olist.order_items` b on a.order_id=b.order_id
-  LEFT JOIN `amplified-cache-495313-g5.Olist.products` c on b.product_id=c.product_id
-  INNER JOIN `amplified-cache-495313-g5.Olist.category_name_translation1` d on c.product_category_name=d.product_category_name
-WHERE a.order_status = 'canceled'
+  LEFT JOIN `amplified-cache-495313-g5.Olist.order_items` b 
+    ON a.order_id = b.order_id
+  LEFT JOIN `amplified-cache-495313-g5.Olist.products` c 
+    ON b.product_id = c.product_id
+  INNER JOIN `amplified-cache-495313-g5.Olist.category_name_translation1` d 
+    ON c.product_category_name = d.product_category_name
+),
+agg AS (
+  SELECT
+    product_name,
+    COUNT(order_id) AS total_orders,
+    COUNTIF(order_status = 'canceled') AS canceled_orders
+  FROM orders_all
+  GROUP BY product_name
 )
--- SELECT
---   count(order_id) as count_product
--- FROM cancellation;
 
 SELECT
   product_name,
-  COUNT(order_id) as count_product
-FROM cancellation
-GROUP BY 1
-ORDER BY 2 DESC;
+  total_orders,
+  canceled_orders,
+  SAFE_DIVIDE(canceled_orders, total_orders) * 100 AS cancellation_rate_pct
+FROM agg
+ORDER BY cancellation_rate_pct DESC;
